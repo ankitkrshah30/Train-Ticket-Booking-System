b1dec2864ddcaeba2f101639b9ae25daa3a2d1c6
ankitkrshah30 <ankitkrshah30@gmail.com>
2025-05-01 23:15:21 +0530

    made chatBot with apiintegration

--- src/main/java/com/ankit/trainTicketBooking/controller/PublicController.java
+++ src/main/java/com/ankit/trainTicketBooking/controller/PublicController.java
@@ -2,13 +2,11 @@ package com.ankit.trainTicketBooking.controller;
 
 import com.ankit.trainTicketBooking.entity.User;
 import com.ankit.trainTicketBooking.service.UserService;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -21,7 +19,7 @@ public class PublicController {
     public UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> createUser(@RequestBody User user){
+    public ResponseEntity<Map<String,Object>> createUser(@RequestBody User user){
         try{
             User savedUser=userService.saveNewUser(user);
             Map<String, Object> response=new LinkedHashMap<>();
@@ -30,7 +28,6 @@ public class PublicController {
             response.put("role",savedUser.getRole());
             return new ResponseEntity<>(response,HttpStatus.CREATED);
         } catch (Exception e) {
-            System.out.println(e);
             Map<String, Object> errorResponse = new LinkedHashMap<>();
             errorResponse.put("message", "Signup failed: Some fields are empty or account already exists.");
             return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
